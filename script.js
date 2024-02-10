6209ceb4544c0f8652283c1ab7ba63e81702bfa8
AMAN KUMAR <kumarsharma2580@gmail.com>
2024-02-11 02:31:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function updateOutput() {
   const outputIframe = document.getElementById('output-iframe');
 
   try {
-    const htmlContent = `${htmlCode}<style>${cssCode}</style><script>${jsCode}</script>`;
-    outputIframe.srcdoc = `<html><head>${cssCode}</head><body>${htmlContent}<script>${jsCode}</script></body></html>`;
+    const htmlContent = `<html><head><style>${cssCode}</style></head><body>${htmlCode}<script>${jsCode}</script></body></html>`;
+    outputIframe.srcdoc = htmlContent;
     outputIframe.classList.remove('error');
   } catch (error) {
     outputIframe.srcdoc = `<span style="color: red;">${error.message}</span>`;
