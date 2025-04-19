eafff71a9d86d225aa74435c23c7dedfc79564c9
AMAN KUMAR <kumarsharma2580@gmail.com>
2025-04-19 21:27:40 +0530

    Update build.sh

--- bot/build.sh
+++ bot/build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Install all dependencies
-pip install -r requirements.txt
-
-# Install Playwright dependencies (including browser binaries)
-python -m playwright install --with-deps
+# Install all required dependencies
+apt-get update
+apt-get install -y \
+    libnss3 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libcups2 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2 \
+    libpangocairo-1.0-0 \
+    libgdk-pixbuf2.0-0 \
+    libpango-1.0-0 \
+    libgtk-3
