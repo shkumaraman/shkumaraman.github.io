d0a73706cc5d19d3aca899a6febd078648274397
AMAN KUMAR <kumarsharma2580@gmail.com>
2025-09-07 18:03:11 +0530

    Update app.py

--- song/app.py
+++ song/app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, Response
 from ytmusicapi import YTMusic
 from flask_cors import CORS
 from pytube import YouTube
-from moviepy.editor import AudioFileClip
-import os
 
 app = Flask(__name__)
 CORS(app)
@@ -32,33 +30,24 @@ def search():
     return jsonify(songs)
 
 @app.route('/audio/<video_id>')
-def get_audio(video_id):
+def stream_audio(video_id):
     url = f'https://www.youtube.com/watch?v={video_id}'
     yt = YouTube(url)
     audio_stream = yt.streams.filter(only_audio=True).first()
-    mp4_file = f"{video_id}.mp4"
-    mp3_file = f"{video_id}.mp3"
-
-    # Download audio
-    audio_stream.download(filename=mp4_file)
-
-    # Convert to mp3
-    clip = AudioFileClip(mp4_file)
-    clip.write_audiofile(mp3_file)
-    clip.close()
-    os.remove(mp4_file)  # remove mp4
-
-    # Serve mp3
-    response = send_file(mp3_file, mimetype="audio/mpeg", as_attachment=False)
-
-    # Delete mp3 after streaming
-    @response.call_on_close
-    def remove_file():
-        os.remove(mp3_file)
-
-    return response
+    
+    # Stream chunks to browser
+    def generate():
+        buffer = audio_stream.stream_to_buffer()
+        while True:
+            chunk = buffer.read(1024*32)
+            if not chunk:
+                break
+            yield chunk
+    
+    return Response(generate(), mimetype="audio/mp4")
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    
