2e07065b9834c253e01a32346c509a0acedc32ce
AMAN KUMAR <kumarsharma2580@gmail.com>
2025-12-14 22:16:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,9 @@ const OFFLINE_URL = "/offline.html";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        OFFLINE_URL,
-        "/",
-        "/index.html"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([OFFLINE_URL, "/", "/index.html"])
+    )
   );
 });
 
@@ -21,17 +17,22 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
         const responseClone = response.clone();
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, responseClone);
         });
+
         return response;
       })
-      .catch(() => {
-        return caches.match(event.request).then(cachedResponse => {
-          return cachedResponse || caches.match(OFFLINE_URL);
-        });
-      })
+      .catch(() =>
+        caches.match(event.request).then(cachedResponse =>
+          cachedResponse || caches.match(OFFLINE_URL)
+        )
+      )
   );
 });
 
